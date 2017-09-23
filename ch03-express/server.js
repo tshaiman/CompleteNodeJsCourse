8beca8aba96dd799af01f20448e677c5909a300c
tshaiman <tomer.shaiman@gmail.com>
2017-09-23 20:19:37 +0300

    ch03

---
+++ ch03-express/server.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+
+var app = express()
+
+//hbs view engine
+hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear() 
+})
+app.set('view engine','hbs')
+
+
+//express middelware
+app.use((req,res,next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log)
+    fs.appendFile('server.log',log + "\n",(err)=>{
+        if(err){
+            console.error("Could not save log to server.log")
+        }
+        
+    })
+    next();
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        welcomeMessage : "Welcome to my web site",
+        pageTitle : "Express Lesson 01-ch03",
+    })
+    
+})
+
+app.get('/about',(req,res) => {
+    res.render('about.hbs',{
+        pageTitle : 'my About Page',
+    })
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server is up on port 3000")
+})
